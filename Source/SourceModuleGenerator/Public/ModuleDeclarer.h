640ba082e3180b76852dc9089f80d39e3c7d7172
U.N.Owen <425571681@qq.com>
2022-02-18 16:41:54 +0800

    Changes structure name ModuleDeclarer to FModuleDeclarer.

--- Source/SourceModuleGenerator/Public/ModuleDeclarer.h
+++ Source/SourceModuleGenerator/Public/ModuleDeclarer.h
@@ -3,17 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ModuleDeclarer.generated.h"
 
-USTRUCT(BlueprintType)
 struct FModuleDeclarer
 {
-	GENERATED_BODY()
-
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "U.N.Owen|ModuleDeclarer|Structure", Meta = (KeyWords = "Notice"))
 	FString CopyrightMessage;
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "U.N.Owen|ModuleDeclarer|Structure", Meta = (KeyWords = "Class Name"))
 	FString ModuleName;
 };
