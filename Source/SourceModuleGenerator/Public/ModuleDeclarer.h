2168458f4ed3be04b39969219f2bc3687ff7c4fa
U.N.Owen <425571681@qq.com>
2022-02-21 18:31:27 +0800

    Add module implement type enumeration.

--- Source/SourceModuleGenerator/Public/ModuleDeclarer.h
+++ Source/SourceModuleGenerator/Public/ModuleDeclarer.h
@@ -4,9 +4,19 @@
 
 #include "CoreMinimal.h"
 
+// Information for creating new module.
 struct FModuleDeclarer
 {
 	FString CopyrightMessage;
 
 	FString ModuleName;
+};
+
+enum class EModuleImplementType : uint8
+{
+	// Regular module implement type.
+	NormalModule = 0,
+
+	// Module that including gameplay code.
+	GameModule,
 };
